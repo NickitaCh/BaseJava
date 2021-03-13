7a25bbccc1792adfce1aed28a28a057b8085dd6b
Nikotinsv <nikandr97@mail.ru>
2021-03-13 12:28:29 +0300

    update 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,4 +1,3 @@
-import javax.xml.ws.ResponseWrapper;
 import java.util.Arrays;
 
 /**
@@ -6,63 +5,56 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int numberOfResumes = 0;
 
     void clear() {
-        for(int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < numberOfResumes; i++) {
             storage[i] = null;
         }
-        q = 0;
+        numberOfResumes = 0;
     }
 
-    int q = 0;
     void save(Resume r) {
-        storage[q] = r;
-        q++;
+        storage[numberOfResumes] = r;
+        numberOfResumes++;
     }
 
     Resume get(String uuid) {
         int i = 0;
-        for (; i < storage.length; i++) {
+        for (; i < numberOfResumes; i++) {
             if (storage[i].uuid == uuid) {
-                break;
+                return storage[i];
             }
         }
-        return storage[i];
+        return null;
     }
 
     void delete(String uuid) {
-        for(int i = 0; i < q; i++) {
+        for (int i = 0; i < numberOfResumes; i++) {
             if (storage[i].uuid == uuid) {
                 storage[i] = null;
             }
         }
-       for (int j = 0; j < q; j++){
-            if (storage[j] == null){
-                for (int k = j + 1; k < q + 1; k++){
+        for (int j = 0; j < numberOfResumes; j++) {
+            if (storage[j] == null) {
+                for (int k = j + 1; k < numberOfResumes + 1; k++) {
                     storage[k - 1] = storage[k];
                 }
-                storage[storage.length - 1] = null;
+                storage[numberOfResumes - 1] = null;
                 break;
             }
         }
-        q--;
+        numberOfResumes--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, q);
+        return Arrays.copyOfRange(storage, 0, numberOfResumes);
     }
 
     int size() {
-        int a = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null)
-            {
-                a++;
-            }
-        }
-        return a;
+        return numberOfResumes;
     }
 }
