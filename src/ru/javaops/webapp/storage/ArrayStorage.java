c732d1ca238d8a1e2694a7ae66a7946ebc5f72c0
Nikotinsv <nikandr97@mail.ru>
2021-03-29 16:08:34 +0300

    update 5

--- src/ru/javaops/webapp/storage/ArrayStorage.java
+++ src/ru/javaops/webapp/storage/ArrayStorage.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int numberOfResumes = 0;
-    private int numberOfResume = 0;
 
     public void clear() {
         Arrays.fill(storage, 0, numberOfResumes, null);
@@ -18,65 +17,53 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (saveCheck(resume) && fullStorageCheck()) {
+        if (checkResume(resume.getUuid()) != -1) {
+            System.out.println("Данное резюме уже находится в базе данных.");
+        } else if (numberOfResumes == 10_000) {
+            System.out.println("База данных заполнена.");
+        } else {
             storage[numberOfResumes] = resume;
             numberOfResumes++;
         }
     }
 
-    private boolean saveCheck(Resume resume) {
+    private int checkResume(String uuid) {
         for (int i = 0; i < numberOfResumes; i++) {
-            if (resume.getUuid().equals(storage[i].getUuid())) {
-                System.out.println("Данное резюме уже находится в базе данных.");
-                return false;
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
             }
         }
-        return true;
-    }
-
-    private boolean fullStorageCheck() {
-        if (numberOfResumes == 10_000) {
-            System.out.println("База данных заполнена.");
-            return false;
-        }
-        return true;
+        return -1;
     }
 
     public Resume get(String uuid) {
-        if (resumeCheck(uuid)) {
-            return storage[numberOfResume];
+        int index = checkResume(uuid);
+        if (index != -1) {
+            return storage[index];
         }
+        System.out.println("Данного резюме нет в базе данных.");
         return null;
     }
 
-    private boolean resumeCheck(String uuid) {
-        for (int i = 0; i < numberOfResumes; i++) {
-            if (uuid.equals(storage[i].getUuid())) {
-                numberOfResume = i;
-                return true;
-            }
-            if (i + 1 == numberOfResumes) {
-                System.out.println("Данного резюме нет в базе данных.");
-            }
-        }
-        return false;
-    }
-
     public void delete(String uuid) {
-        if (resumeCheck(uuid)) {
-            storage[numberOfResume] = null;
-            for (int k = numberOfResume + 1; k < numberOfResumes + 1; k++) {
+        int index = checkResume(uuid);
+        if (index != -1) {
+            storage[index] = null;
+            for (int k = index + 1; k < numberOfResumes + 1; k++) {
                 storage[k - 1] = storage[k];
             }
             storage[numberOfResumes - 1] = null;
             numberOfResumes--;
-        }
+        } else
+            System.out.println("Данного резюме нет в базе данных.");
     }
 
     public void update(Resume resume) {
-        if (resumeCheck(resume.getUuid())) {
-            storage[numberOfResume].setUuid(resume.getUuid());
-        }
+        int index = checkResume(resume.getUuid());
+        if (checkResume(resume.getUuid()) == -1) {
+            System.out.println("Данного резюме нет в базе данных.");
+        } else
+            storage[index].setUuid(resume.getUuid());
     }
 
     /**
